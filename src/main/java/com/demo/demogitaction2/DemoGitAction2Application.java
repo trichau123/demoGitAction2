c80f0d2e8e2facdeb8d5c062371d4c2b90e29936
trichau123 <53028788+trichau123@users.noreply.github.com>
2023-06-14 19:51:52 -0400

    update main

--- src/main/java/com/demo/demogitaction2/DemoGitAction2Application.java
+++ src/main/java/com/demo/demogitaction2/DemoGitAction2Application.java
@@ -14,6 +14,7 @@ public class DemoGitAction2Application {
         return "Hello";
     }
     public static void main(String[] args) {
+        System.out.println("HELLO");
         SpringApplication.run(DemoGitAction2Application.class, args);
     }
 
